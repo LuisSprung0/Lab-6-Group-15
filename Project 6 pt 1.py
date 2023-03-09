bf349f8f06c8d490e907f4957b24845af7c79626
LuisSprung0 <lsprung@ufl.edu>
2023-03-08 23:14:17 -0500

    Full encode/decode

--- Project 6 pt 1.py
+++ Project 6 pt 1.py
@@ -1,27 +1,49 @@
-global encoded_pass
-encoded_pass = 0
+# Luis Sprung
+# COP3502 Programming Fundamentals 1
+# UFID: 91487486 
 
-def menu_Option(num):
-    global password_inp
-    global encoded_pass
-    if num == 1:
-        password_inp = int(input('Please enter your password to encode: '))
-        encoded_pass = (password_inp*2)^2 + 6
-        print('Your password has been encoded and stored!')
+# Encoding and decoding functions #
+def encode(password):
+    enc_password = ""
+    for char in password:
+        enc_char = str((int(char) + 5) % 10)
+        enc_password += enc_char
+    return enc_password
 
-    elif num == 2:
-        pass
+def decode(enc_password):
+    dec_password = ""
+    for char in enc_password:
+        dec_char = str((int(char) - 5) % 10)
+        dec_password += dec_char
+    return dec_password
 
-    elif num == 3:
+# Main functioning code #
+def main():
+    print("Menu\n-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    
+# Menu options #
+    option = int(input("\nPlease enter an option: "))
+    if option == 1:
+        password = input("Please enter your 8-digit password to encode: ")
+        print("Your encoded password is:", encode(password))
+        print()
+        main()
+
+    elif option == 2:
+        enc_password = input("Please enter your previously encoded 8-digit password: ")
+        print("Your decoded password is:", decode(enc_password))
+        print()
+        main()
+
+    elif option == 3:
         quit
 
+    else:
+        print("Invalid option, Try again.")
+        main()
 
-def main():
-    print("Menu" 
-          + "\n-------------" 
-          + "\n1. Encode\n2. Decod\n3. Quit") 
-    num = int(input('\nPlease enter an option: '))
-    menu_Option(num)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
